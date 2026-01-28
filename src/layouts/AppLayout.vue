d61d8b582efd3f4deeda1b6c73a495e5f17e78d9
Sabyrka <zhaslanulysa6yrka@gmail.com>
2026-01-28 19:42:55 +0500

    feat: enhance AppLayout styles with improved header structure and title styling

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -19,16 +19,25 @@ import AppMenu from '@/components/AppMenu.vue'
 </template>
 
 <style lang="scss" scoped>
+@import '@/assets/styles/index.scss';
+
 .root {
   display: flex;
 
   .main {
     flex: 1;
+    padding-top: 20px;
     padding-inline: 16px;
 
     .header {
       display: flex;
       justify-content: space-between;
+
+      .title {
+        color: $violet;
+        font-size: 28px;
+        font-weight: 700;
+      }
     }
   }
 }
