189807eaf1f88f7e02fe3b407d13660ea9e334e0
Sabyrka <zhaslanulysa6yrka@gmail.com>
2026-01-28 20:36:34 +0500

    feat: implement recipe fetching logic in HomePage component

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,23 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
 import AppLayout from '@/layouts/AppLayout.vue'
 import AppButton from '@/components/AppButton.vue'
+
+import { RecipesService } from '@/services'
+
+const recipes = ref([])
+
+const fetchRecipes = async () => {
+  try {
+    const response = await RecipesService.getRecipesByLetter()
+    recipes.value = response.data
+  } catch (error) {
+    console.error('Error fetching recipes:', error)
+  }
+}
+
+onMounted(fetchRecipes)
 </script>
 
 <template>
